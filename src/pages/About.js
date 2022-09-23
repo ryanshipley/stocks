347b390409e2b724f411b3a07285179711ef9f22
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-22 22:04:15 -0400

    adds image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function About(props){
     return(
         <div>
-            <h1>About</h1>
+            <h1>We Display Stonk</h1>
+            <Link to="/stocks">
+                Check out these stonks
+            </Link>
+            <div>
+                <img src="https://i.ytimg.com/vi/SMRg9mKcyhM/mqdefault.jpg" />
+            </div>
         </div>
     );
 };
