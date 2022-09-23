285a42d2d0397a5df014c8932595a5eefb819c1a
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-22 21:57:53 -0400

    adds to stocks link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home(props){
     return(
         <div>
             <h1>Home</h1>
+            <Link to="/stocks">
+                Check out the stocks
+            </Link>
         </div>
     );
 };
