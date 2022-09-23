0719434d2ecc6b188f53349beaee1c85f08d4275
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-22 21:16:31 -0400

    fixes erroe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Route, Swtich } from "react-router-dom";
+import Stocks from "./pages/Stock";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
